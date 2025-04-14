18941391f10fd83102c949bf0b9ca3e2d391e17f
BuildTools <igorjeremic128@gmail.com>
2025-04-15 01:40:16 +0200

    update

--- CS322-PZ01-IgorJeremic5385/projkat/components/RequestServiceComponent.cs
+++ CS322-PZ01-IgorJeremic5385/projkat/components/RequestServiceComponent.cs
@@ -18,14 +18,23 @@ namespace projkat
         }
 
         private void InitializeUI()
-        {
+        {     
             GroupBox groupBox = new GroupBox
             {
                 Text = "Service Request",
                 Size = new System.Drawing.Size(400, 300),
                 Location = new System.Drawing.Point(10, 10)
             };
+            
             this.Controls.Add(groupBox);
+
+            this.Dock = DockStyle.Fill;
+
+            groupBox.Location = new System.Drawing.Point(
+                this.ClientSize.Width - 100 / 2 ,
+                this.ClientSize.Height - 60 / 2
+            );
+
 
             Label fullNameLabel = new Label
             {
@@ -61,11 +70,28 @@ namespace projkat
             };
             groupBox.Controls.Add(emailTextBox);
 
+            Label mechEmailLabel = new Label
+            {
+                Text = "Mech Email:",
+                ForeColor = System.Drawing.Color.White,
+                Location = new System.Drawing.Point(10, 110),
+                Size = new System.Drawing.Size(100, 20)
+            };
+            groupBox.Controls.Add(mechEmailLabel);
+
+            TextBox mechEmailTextBox = new TextBox
+            {
+                Name = "mechEmailTextBox",
+                Location = new System.Drawing.Point(130, 110),
+                Size = new System.Drawing.Size(250, 20)
+            };
+            groupBox.Controls.Add(mechEmailTextBox);
+
             Label problemTypeLabel = new Label
             {
                 Text = "Problem Type:",
                 ForeColor = System.Drawing.Color.White,
-                Location = new System.Drawing.Point(10, 110),
+                Location = new System.Drawing.Point(10, 150),
                 Size = new System.Drawing.Size(100, 20)
             };
             groupBox.Controls.Add(problemTypeLabel);
@@ -73,7 +99,7 @@ namespace projkat
             TextBox problemTypeTextBox = new TextBox
             {
                 Name = "problemTypeTextBox",
-                Location = new System.Drawing.Point(130, 110),
+                Location = new System.Drawing.Point(130, 150),
                 Size = new System.Drawing.Size(250, 20)
             };
             groupBox.Controls.Add(problemTypeTextBox);
@@ -82,7 +108,7 @@ namespace projkat
             {
                 Text = "Problem Description:",
                 ForeColor = System.Drawing.Color.White,
-                Location = new System.Drawing.Point(10, 150),
+                Location = new System.Drawing.Point(10, 190),
                 Size = new System.Drawing.Size(120, 20)
             };
             groupBox.Controls.Add(problemDescriptionLabel);
@@ -90,33 +116,17 @@ namespace projkat
             TextBox problemDescriptionTextBox = new TextBox
             {
                 Name = "problemDescriptionTextBox",
-                Location = new System.Drawing.Point(130, 150),
+                Location = new System.Drawing.Point(130, 190),
                 Multiline = true,
                 Size = new System.Drawing.Size(250, 50)
             };
             groupBox.Controls.Add(problemDescriptionTextBox);
 
-            Label isDoneLabel = new Label
-            {
-                Text = "Is Done:",
-                ForeColor = System.Drawing.Color.White,
-                Location = new System.Drawing.Point(10, 210),
-                Size = new System.Drawing.Size(100, 20)
-            };
-            groupBox.Controls.Add(isDoneLabel);
-
-            TextBox isDoneTextBox = new TextBox
-            {
-                Name = "isDoneTextBox",
-                Location = new System.Drawing.Point(130, 210),
-                Size = new System.Drawing.Size(250, 20)
-            };
-            groupBox.Controls.Add(isDoneTextBox);
             Button submitButton = new Button {
                 Name = "submitButton",
                 Text = "submit",
                 Size = new System.Drawing.Size(70, 25),
-                Location = new System.Drawing.Point(150, 250),
+                Location = new System.Drawing.Point(310, 250),
                 ForeColor = System.Drawing.Color.White,
 
             };
